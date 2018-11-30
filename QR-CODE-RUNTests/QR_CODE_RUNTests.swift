199af3afc386acc9322644f001f21a3edd5a056c
ueaoiii130 <aoiue.w.bbb@gmail.com>
2018-12-01 01:34:55 +0900

    Initial Commit

---
+++ QR-CODE-RUNTests/QR_CODE_RUNTests.swift
@@ -0,0 +1,34 @@
+//
+//  QR_CODE_RUNTests.swift
+//  QR-CODE-RUNTests
+//
+//  Created by 阪上葵 on 2018/12/01.
+//  Copyright © 2018 ueao. All rights reserved.
+//
+
+import XCTest
+@testable import QR_CODE_RUN
+
+class QR_CODE_RUNTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
